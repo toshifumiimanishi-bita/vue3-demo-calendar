7fee0f8def344930d0c48137389e05be5ffc1b08
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2020-12-31 16:57:41 +0900

    カレンダータイトルコンポーネントの新規作成

---
+++ src/components/atoms/CalendarTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="$style.title">{{ currentYear }}年{{ currentMonth }}月</div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentYear: {
+      type: Number,
+      default: 0,
+    },
+    currentMonth: {
+      type: Number,
+      default: 0,
+    },
+  },
+}
+</script>
+
+<style lang="scss" module>
+.title {
+  font-size: 20px;
+}
+</style>
