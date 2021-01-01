e0e4b15808f0b23f8c9f993bd99e41dc811ec50b
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-01 12:13:46 +0900

    今日ボタンのコンポーネントの新規作成

---
+++ src/components/atoms/CalendarTodayButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button :class="$style.todaybutton" type="button" @click="handleClick">今日</button>
+</template>
+
+<script>
+export default {
+  emits: {
+    'init:date': null,
+  },
+  setup(props, { emit }) {
+    const handleClick = () => {
+      emit('init:date');
+    };
+
+    return {
+      handleClick,
+    };
+  },
+}
+</script>
+
+<style lang="scss" module>
+.todaybutton {
+  background-color: transparent;
+  border: 1px solid #dadce0;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 12px;
+  height: 100%;
+  line-height: 1;
+  padding: 8px;
+}
+</style>
