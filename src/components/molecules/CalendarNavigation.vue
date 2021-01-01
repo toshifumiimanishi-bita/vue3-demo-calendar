48e62d031a4e9c76ec655cfcd582cc01c486f8dd
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-01 19:57:13 +0900

    「前月」、「次月」ナビゲーションコンポーネントの新規作成

---
+++ src/components/molecules/CalendarNavigation.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <button :class="$style.prevbutton" @click="handlePrevButtonClick">＜</button>
+    <button :class="$style.nextbutton" @click="handleNextButtonClick">＞</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  emits: {
+    prev: null,
+    next: null,
+  },
+  setup(props, { emit }) {
+    const handlePrevButtonClick = () => {
+      emit('prev');
+    };
+    const handleNextButtonClick = () => {
+      emit('next');
+    };
+
+    return {
+      handlePrevButtonClick,
+      handleNextButtonClick,
+    };
+  },
+}
+</script>
+
+<style lang="scss" module>
+.prevbutton,
+.nextbutton {
+  background: none;
+  border: 0;
+  font-size: 16px;
+  font-weight: bold;
+}
+</style>
