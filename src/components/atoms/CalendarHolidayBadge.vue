e40a539a4756947a658124bfba2dd994057cbaad
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-12 15:18:22 +0900

    「祝日バッジ」コンポーネントの新規作成

---
+++ src/components/atoms/CalendarHolidayBadge.vue
@@ -0,0 +1,28 @@
+<template>
+  <AppBadge :badge-label="holiday" :style-object="styleObject" />
+</template>
+
+<script>
+import AppBadge from './AppBadge.vue';
+
+export default {
+  components: {
+    AppBadge,
+  },
+  props: {
+    holiday: {
+      type: String,
+      default: '',
+      required: true,
+    },
+  },
+  setup(props) {
+    const styleObject = {
+      backgroundColor: '#f9a802',
+    };
+    return {
+      styleObject,
+    };
+  },
+};
+</script>
