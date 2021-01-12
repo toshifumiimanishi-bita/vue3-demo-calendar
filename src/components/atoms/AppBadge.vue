96df9e7a971c0157022621d25c696e4ca164475e
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-12 15:15:21 +0900

    基底バッジコンポーネントの新規作成

---
+++ src/components/atoms/AppBadge.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="$style.badge" :style="styleObject">{{ badgeLabel }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    badgeLabel: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    styleObject: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+.badge {
+  background-color: #ccc;
+  border-radius: 4px;
+  color: #fff;
+  font-size: 12px;
+  font-weight: bold;
+  line-height: 1.4;
+  margin: 4px 0;
+  overflow: hidden;
+  padding: 3px 8px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
