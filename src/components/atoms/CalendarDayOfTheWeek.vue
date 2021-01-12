cc7b58b1360569186e103d36fb65192c44134367
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-12 14:47:57 +0900

    曜日コンポーネントの新規作成

---
+++ src/components/atoms/CalendarDayOfTheWeek.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="$style.dayoftheweek">{{ dayOfTheWeek }}</div>
+</template>
+
+<script>
+import useDayOfTheWeek from '../../composables/use-day-of-the-week';
+
+export default {
+  props: {
+    dayOfTheWeekIndex: {
+      type: Number,
+      default: 0,
+    },
+  },
+  setup({ dayOfTheWeekIndex }) {
+    const dayOfTheWeek = useDayOfTheWeek(dayOfTheWeekIndex);
+    return {
+      dayOfTheWeek,
+    };
+  },
+}
+</script>
+
+<style lang="scss" module>
+.dayoftheweek {
+  font-size: 10px;
+  font-weight: normal;
+  padding: 5px 0;
+  text-align: center;
+  width: 100%;
+}
+</style>
