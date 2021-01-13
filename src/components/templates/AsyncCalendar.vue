74a1714375576e441db747de7d9952fe597ffc79
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-13 15:00:36 +0900

    非同期カレンダーコンポーネントの新規作成

---
+++ src/components/templates/AsyncCalendar.vue
@@ -0,0 +1,21 @@
+<template>
+  <Calendar :holidays="holidays" />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import fetchHolidays from '../../utils/fetchHolidays';
+import Calendar from './Calendar.vue';
+
+export default defineComponent ({
+  components: {
+    Calendar,
+  },
+  async setup() {
+    const holidays = await fetchHolidays();
+    return {
+      holidays,
+    };
+  }
+});
+</script>
