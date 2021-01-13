6a47b3136993a4ad274d00f5295afd8e5e7fab39
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-13 09:01:09 +0900

    曜日を返すユーティリティ関数の新規作成

---
+++ src/utils/getDayOfTheWeek.js
@@ -0,0 +1,5 @@
+export default function getDayOfTheWeek(dayOfTheWeekIndex) {
+  const dayOfTheWeeks = ['日', '月', '火', '水', '木', '金', '土'];
+  const dayOfTheWeek = dayOfTheWeeks[dayOfTheWeekIndex];
+  return dayOfTheWeek;
+}
