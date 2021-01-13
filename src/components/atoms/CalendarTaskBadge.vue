2dc46e99cd1bb7767fcf30f8b8ad453dc960a964
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-13 16:40:37 +0900

    タスクバッジコンポーネントの新規作成

---
+++ src/components/atoms/CalendarTaskBadge.vue
@@ -0,0 +1,49 @@
+<template>
+  <AppBadge :class="$style.badge" :style-object="styleObject">
+    {{ taskName }}
+    <button :class="$style.badge_closebutton" type="button">×</button>
+  </AppBadge>
+</template>
+
+<script>
+import AppBadge from './AppBadge.vue';
+
+export default {
+  components: {
+    AppBadge,
+  },
+  props: {
+    taskId: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    taskName: {
+      type: String,
+      default: '',
+      required: true,
+    },
+  },
+  setup() {
+    const styleObject = {
+      backgroundColor: '#2ac8e2',
+    };
+    return {
+      styleObject,
+    };
+  },
+}
+</script>
+
+<style lang="scss" module>
+.badge {
+  position: relative;
+}
+
+.badge_closebutton {
+  position: absolute;
+  right: 8px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+</style>
