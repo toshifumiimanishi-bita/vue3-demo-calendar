282445bd791eb45cfef4f3ac5cab6d5b745772d4
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-14 14:53:06 +0900

    基底入力欄コンポーネントの新規作成

---
+++ src/components/atoms/AppTextFileld.vue
@@ -0,0 +1,37 @@
+<template>
+  <input
+    :class="$style.textfiled"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    type="text"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+  emits: {
+    'update:modelValue': (value) => true,
+  }
+}
+</script>
+
+<style lang="scss" module>
+.textfiled {
+  background-color: transparent;
+  border: 0;
+  border-bottom: 1px solid var(--base-border-color);
+  font-size: 15px;
+  padding: 0.5em;
+}
+</style>
