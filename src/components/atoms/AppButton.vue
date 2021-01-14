d982312e98706b784bd62c086b6f47386a42de25
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-14 14:59:49 +0900

    基底ボタンコンポーネントの新規作成

---
+++ src/components/atoms/AppButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    :class="$style.button"
+    :style="styleObject"
+    type="button"
+  >{{ label }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    styleObject: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+.button {
+  background-color: #ccc;
+  border-radius: var(--base-border-radius);
+  color: #fff;
+  font-size: 12px;
+  line-height: 1;
+  padding: 12px 8px;
+}
+</style>
