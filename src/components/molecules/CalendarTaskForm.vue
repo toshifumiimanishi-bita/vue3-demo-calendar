6368953930d530c21f1061bd2c449e0e4e9a13b2
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-14 18:10:51 +0900

    タスク入力フォームコンポーネントの新規作成

---
+++ src/components/molecules/CalendarTaskForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <form class="form" @submit.prevent="saveTask">
+    <div :class="$style.form_textfield">
+      <AppTextFileld
+        v-model="newTaskName"
+        placeholder="タスクを入力"
+      />
+    </div>
+    <AppButton
+      :class="$style.form_savebutton"
+      :style-object="styleObject"
+      :disabled="isSaveButtonDisabled"
+      type="submit"
+      label="保存"
+    />
+  </form>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+import AppButton from '../atoms/AppButton.vue';
+import AppTextFileld from '../atoms/AppTextFileld.vue';
+
+export default {
+  components: {
+    AppButton,
+    AppTextFileld,
+  },
+  emits: {
+    save: (value) => true,
+  },
+  setup(props, { emit }) {
+    const newTaskName = ref('');
+    const isSaveButtonDisabled = computed(() => {
+      return newTaskName.value === '';
+    });
+    const styleObject = {
+      backgroundColor: '#1a73e8',
+    };
+    const saveTask = () => {
+      emit('save', newTaskName.value);
+      newTaskName.value = '';
+    };
+
+    return {
+      newTaskName,
+      isSaveButtonDisabled,
+      styleObject,
+      saveTask,
+    };
+  }
+}
+</script>
+
+<style lang="scss" module>
+.form_textfield {
+  text-align: center;
+}
+
+.form_savebutton {
+  bottom: 8px;
+  position: absolute;
+  right: 8px;
+  width: 100px;
+}
+</style>
