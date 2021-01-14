305c8f3874b70ed780e26e0424a4223ef35b0f46
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-14 19:02:43 +0900

    タスク入力フォームモーダルコンポーネントの新規作成

---
+++ src/components/organisms/CalendarTaskFormModal.vue
@@ -0,0 +1,64 @@
+<template>
+  <AppModal :is-visible="isVisible">
+    <p :class="$style.modal_title">{{ modalTitle }}</p>
+    <CalendarTaskForm @save="handleTaskSave" />
+    <button
+      :class="$style.modal_closebutton"
+      type="button"
+      aria-label="close"
+      @click="$emit('hide')"
+    >×</button>
+  </AppModal>
+</template>
+
+<script>
+import CalendarTaskForm from '../molecules/CalendarTaskForm.vue';
+import AppModal from './AppModal.vue';
+
+export default {
+  components: {
+    CalendarTaskForm,
+    AppModal,
+  },
+  props: {
+    isVisible: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    modalTitle: {
+      type: String,
+      default: '',
+      required: true,
+    },
+  },
+  emits: {
+    save: (newTaskName) => true,
+    hide: null,
+  },
+  setup(props, { emit }) {
+    const handleTaskSave = (newTaskName) => {
+      emit('save', newTaskName);
+      emit('hide');
+    };
+    return {
+      handleTaskSave,
+    };
+  },
+}
+</script>
+
+<style lang="scss" module>
+.modal_title {
+  font-size: 15px;
+  margin-bottom: 24px;
+  text-align: center;
+}
+
+.modal_closebutton {
+  position: absolute;
+  right: 6px;
+  top: 6px;
+  width: 40px;
+}
+</style>
