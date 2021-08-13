3dadbe7776b7336f595b23bcd1e987760302d2f2
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-08-13 20:11:01 +0900

    タイポ修正

--- src/components/atoms/AppTextFileld.vue
+++ src/components/atoms/AppTextFileld.vue
@@ -1,6 +1,6 @@
 <template>
   <input
-    :class="$style.textfiled"
+    :class="$style.textfield"
     :placeholder="placeholder"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
@@ -27,7 +27,7 @@ export default {
 </script>
 
 <style lang="scss" module>
-.textfiled {
+.textfield {
   background-color: transparent;
   border: 0;
   border-bottom: 1px solid var(--base-border-color);
